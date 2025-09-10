564a0ad56fef86d0113219d3a83e2600cdc068ab
Matt <mmyers75@icloud.com>
2025-09-10 06:42:40 -0400

    feat(haywardomnilogiclocal): handle compression flag in multi-block responses

--- bundles/org.openhab.binding.haywardomnilogiclocal/src/main/java/org/openhab/binding/haywardomnilogiclocal/internal/net/UdpClient.java
+++ bundles/org.openhab.binding.haywardomnilogiclocal/src/main/java/org/openhab/binding/haywardomnilogiclocal/internal/net/UdpClient.java
@@ -73,10 +73,8 @@ public class UdpClient {
                 buffer.getLong();
                 buffer.position(16);
                 int msgType = buffer.getInt();
-                buffer.get();
-                int blockCount = Byte.toUnsignedInt(buffer.get());
-                boolean thisCompressed = buffer.get() != 0;
-                buffer.get();
+                int blockCount = Byte.toUnsignedInt(data[21]);
+                boolean thisCompressed = data[22] == 1;
 
                 if (!ackSent && (msgType == MSG_LEAD || msgType == MSG_BLOCK
                         || msgType == HaywardMessageType.MSP_TELEMETRY_UPDATE.getMsgInt())) {
@@ -88,6 +86,7 @@ public class UdpClient {
                     expectedBlocks = blockCount;
                     compressed = thisCompressed;
                 } else if (msgType == MSG_BLOCK) {
+                    compressed |= thisCompressed;
                     blocks.write(data, 24, data.length - 24);
                     expectedBlocks--;
                     if (expectedBlocks == 0) {
